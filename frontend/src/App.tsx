8d7d8d1d2fdae66e2978069858aff02bc067944b
Scott Sanford <ssanford@mediafly.com>
2022-04-15 22:53:48 -0500

    replace with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const API_URL = 'https://xw8i25bu04.execute-api.us-east-1.amazonaws.com/Prod/get'
+
+function App() {
+
+  const [ visitorCount, setVisitorCount ] = useState<number>()
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((document) => {
+        setVisitorCount(document.Item.visitorCount)
+      })
+
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        {visitorCount && <div>Count { visitorCount }</div>}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
