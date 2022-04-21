1e9c56ddb088fc9b08ea729ec5eb5236178714af
Scott Sanford <ssanford@mediafly.com>
2022-04-21 15:54:53 -0500

    update api url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const API_URL = 'https://xw8i25bu04.execute-api.us-east-1.amazonaws.com/Prod/get'
+const API_URL = ' https://x534x9hk9k.execute-api.us-east-1.amazonaws.com/Prod/get'
 
 function App() {
 
