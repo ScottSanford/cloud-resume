ac403b054a95dc0dac9d2a35bd119ef2b2028159
Scott Sanford <ssanford@mediafly.com>
2022-04-23 20:37:31 -0500

    Fix issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Resume from './components/Resume/Resume'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -9,20 +9,13 @@ const API_URL = 'https://x534x9hk9k.execute-api.us-east-1.amazonaws.com/Prod/get
 function App() {
 
   const [visitorCount, setVisitorCount] = useState<number>()
-  // const isInitialRender = useRef(true)
 
   useEffect(() => {
-    // prevent useEffect from being called twice
-    // if (isInitialRender.current) {
-    //   isInitialRender.current = false
-    //   return
-    // }
     fetch(API_URL)
       .then((response) => response.json())
       .then((document) => {
         setVisitorCount(document.Attributes.visitorCount)
       })
-
   }, [])
 
   return (
